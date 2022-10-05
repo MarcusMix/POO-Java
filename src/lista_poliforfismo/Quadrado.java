d5ee21b8c313d2bbe4a1b100c30697741aa41889
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-05 15:55:32 -0300

    correção de bugs

---
+++ src/lista_poliforfismo/Quadrado.java
@@ -0,0 +1,42 @@
+package lista_poliforfismo;
+
+public class Quadrado {
+	private int lado;
+	private String nome;
+	
+	public Quadrado(int lado, String nome) {
+		super();
+		this.lado = lado;
+		this.nome = nome;
+	}
+
+	public Quadrado() {
+		super();
+	}
+
+	public int getLado() {
+		return lado;
+	}
+
+	public void setLado(int lado) {
+		this.lado = lado;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public double calcularArea(int lado) {
+		return lado * lado;
+	}
+	
+	
+	public double calcularPerimetro(int lado) {
+		return lado * 4;
+	}
+}
