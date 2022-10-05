891ce93a0c4b19ea636cb1f5d8bb809d8b7bd5d8
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-05 16:02:43 -0300

    classe Circulo criada junto com seus métodos e connstrutores

---
+++ src/lista_poliforfismo/Circulo.java
@@ -0,0 +1,44 @@
+package lista_poliforfismo;
+
+public class Circulo {
+	private double raio;
+	private String nome;
+	
+	public Circulo(double raio, String nome) {
+		super();
+		this.raio = raio;
+		this.nome = nome;
+	}
+
+	public Circulo() {
+		super();
+	}
+
+	public double getRaio() {
+		return raio;
+	}
+
+	public void setRaio(double raio) {
+		this.raio = raio;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+	public double calcularArea(double raio) {
+		return 3.14 * (raio * 2);
+	}
+	
+	
+	public double calcularPerimetro(double raio) {
+		return 2 * 3.14 * raio;
+	}
+}
+	
