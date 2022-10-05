cefd98655e98d822a46549c7c55625f6143ad09c
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-05 16:13:13 -0300

    criado Classe retangulo seus atributos, construtores e métodos

--- src/lista_poliforfismo/Retangulo.java
+++ src/lista_poliforfismo/Retangulo.java
@@ -1,5 +1,53 @@
 package lista_poliforfismo;
 
 public class Retangulo {
+	private double ladoA;
+	private double ladoB;
+	private String nome;
+	
+	public Retangulo(double ladoA, double ladoB, String nome) {
+		super();
+		this.ladoA = ladoA;
+		this.ladoB = ladoB;
+		this.nome = nome;
+	}
 
+	public Retangulo() {
+		super();
+	}
+
+	public double getLadoA() {
+		return ladoA;
+	}
+
+	public void setLadoA(double ladoA) {
+		this.ladoA = ladoA;
+	}
+
+	public double getLadoB() {
+		return ladoB;
+	}
+
+	public void setLadoB(double ladoB) {
+		this.ladoB = ladoB;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	public double calcularArea(double ladoA, double ladoB) {
+		return ladoA * ladoB;
+	}
+	
+	
+	public double calcularPerimetro(int lado) {
+		return  ladoA * ladoB;
+	}
+	
 }
