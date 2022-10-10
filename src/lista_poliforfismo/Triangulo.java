db7753a07b5541decd230f6f756782c1382c934a
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-10 13:23:52 -0300

    criação classe Triangulo

--- src/lista_poliforfismo/Triangulo.java
+++ src/lista_poliforfismo/Triangulo.java
@@ -1,5 +1,53 @@
 package lista_poliforfismo;
 
 public class Triangulo {
+	private double base;
+	private double altura;
+	private String nome;
+	
+	public Triangulo(double base, double altura, String nome) {
+		super();
+		this.base = base;
+		this.altura = altura;
+		this.nome = nome;
+	}
 
+	public Triangulo() {
+		super();
+	}
+
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+
+	public double calcularArea(double base, double altura) {
+		return (base * altura) / 2;
+	}
+	
+	
+	public double calcularPerimetro(double raio) {
+		return base + (altura * 2);
+	}
+	
 }
