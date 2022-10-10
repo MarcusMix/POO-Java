e2e6f6e2dd554ac53695df01d1726097fa72c5e5
Marcus Sandi <tradedomarcus@gmail.com>
2022-10-10 14:18:20 -0300

    criado classe geometricas abstrata

---
+++ src/lista_poliforfismo/ClassesGeometricas.java
@@ -0,0 +1,10 @@
+package lista_poliforfismo;
+
+public abstract class  ClassesGeometricas {
+	private String nome;
+	
+	public abstract double calcularArea(double raio);
+	
+	
+	public abstract double calcularPerimetro(double raio) ;
+}
