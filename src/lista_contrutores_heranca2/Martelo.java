5bd6e6555a0e3d43c22b818676df0d8b6e47245a
Marcus Sandi <tradedomarcus@gmail.com>
2022-11-07 13:36:29 -0300

    feat: add class martelo

---
+++ src/lista_contrutores_heranca2/Martelo.java
@@ -0,0 +1,12 @@
+package lista_contrutores_heranca2;
+
+public class Martelo extends FerramentaManual {
+
+	public Martelo() {
+		super();
+	}
+
+	public Martelo(String modelo, String fabricante, String tipo, double tamanho) {
+		super(modelo, fabricante, tipo, tamanho);
+	}
+}
