7c2c45e354bec2ffbea7f5c465817cabdf7b203d
Marcus Sandi <tradedomarcus@gmail.com>
2022-11-07 13:37:19 -0300

    feat: add class serrote

---
+++ src/lista_contrutores_heranca2/Serrote.java
@@ -0,0 +1,12 @@
+package lista_contrutores_heranca2;
+
+public class Serrote extends FerramentaManual {
+
+	public Serrote() {
+		super();
+	}
+
+	public Serrote(String modelo, String fabricante, String tipo, double tamanho) {
+		super(modelo, fabricante, tipo, tamanho);
+	}
+}
